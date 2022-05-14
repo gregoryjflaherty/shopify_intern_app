2e5437a4d457e072c980db5ce063233797133846
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-05-14 12:03:45 -0400

    Feat: update action in items controller, find item method, items param

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :get_item, only: [:update]
+
   def index
     @items = Item.alphabetically
   end
@@ -11,10 +13,21 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item.update(item_params)
+    flash[:notice] = "#{@item.name} Has Been Updated!"
+    redirect_to items_path
   end
 
   def destroy
   end
 
-  
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description,:warehouse_id)
+  end
+
+  def get_item
+    @item = Item.find_by(id: params[:id])
+  end
 end
