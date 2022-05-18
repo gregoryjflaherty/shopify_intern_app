d0f1572b4594724e1f793b6bc2f6106e610cbe59
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-05-17 20:58:37 -0400

    Test: add validation tests for name and description

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Item do
   describe 'associations' do
       it { should belong_to(:warehouse).optional }
+      it { should validate_presence_of(:name)}
+      it { should validate_presence_of(:description)}
   end
 
   describe '#class methods' do
