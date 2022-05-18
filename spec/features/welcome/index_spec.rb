bb8094a61d215995bbfe6b268e98df7fb20d23d6
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-05-17 20:29:01 -0400

    Test: add xpath testing for further accuracy

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -14,16 +14,20 @@ RSpec.describe 'Landing Page' do
  it 'has items button' do
    expect(current_path).to eq(root_path)
 
-   expect(page).to have_button("Items")
-   click_on "Items"
-   expect(current_path).to eq(items_path)
+   within(:xpath, "/html/body/center") do
+    expect(page).to have_button("Items")
+    click_on "Items"
+    expect(current_path).to eq(items_path)
+   end
+
  end
 
  it 'has warehouses button' do
    expect(current_path).to eq(root_path)
-
-   expect(page).to have_button("Warehouses")
-   click_on "Warehouses"
-   expect(current_path).to eq(warehouses_path)
+   within(:xpath, "/html/body/center") do
+     expect(page).to have_button("Warehouses")
+     click_on "Warehouses"
+     expect(current_path).to eq(warehouses_path)
+   end
  end
 end
